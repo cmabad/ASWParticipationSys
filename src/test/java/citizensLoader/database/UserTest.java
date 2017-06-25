b4cef50b92bb1618e745fb0c97a7b7fec47c3563
katiaff <katia.f.f4@gmail.com>
2017-06-25 22:24:02 +0200

    ok

--- src/test/java/citizensLoader/database/UserTest.java
+++ src/test/java/citizensLoader/database/UserTest.java
@@ -13,18 +13,6 @@ public class UserTest {
 	
 	UserDao userdao = new UserDao();
 
-	@Test
-	public void saveUserTest() throws ParseException {
-		
-		User newUser = new User("katia", "fernandez", "katia@uniovi.es", "27-06-1995", "oviedo", 
-			 "España", 7190060);
-		
-		userdao.saveUser(newUser);
-		
-		User lookForNewUser = userdao.getUserByEmail("nuevousuario@uniovi.es");
-		
-		assertEquals(newUser, lookForNewUser);
-	}
 	
 	@Test
 	public void getUserByEmailTest() throws ParseException {
@@ -36,13 +24,6 @@ public class UserTest {
 		int dniFound = userByEmail.getId();
 		
 		assertEquals(dniFound, dniUser);
-		
-		int dniUser2 = 7190060;
-		
-		User userByEmail2 = userdao.getUserByEmail("katia@uniovi.es");
-		int dniFound2 = userByEmail2.getId();
-		
-		assertEquals(dniFound2, dniUser2);
 	}
 	
 	
